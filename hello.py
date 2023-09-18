73861cd90f815c1c0c77c3e43ec7aef82eec34fc
arshar2411 <arjunmsharm@gmail.com>
2023-09-18 16:25:12 -0400

    example 2-2, hello name

--- hello.py
+++ hello.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}!</h1>'
+
+# if __name__ == '__main__':
+#     app.run()
