36c48f40943dfdcd5ed47e7daa0d7312f0858654
arshar2411 <arjunmsharm@gmail.com>
2023-09-19 15:47:44 -0400

    step 1, reproduce 4-7

--- hello.py
+++ hello.py
@@ -1,21 +1,36 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from datetime import datetime
 
+from flask import Flask, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import EmailField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "shhhh ... "
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route("/",methods=['GET','POST'])
 def index():
-    return render_template('index.html',name='Arjun',current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<name>')
+
+@app.route("/user/<name>")
 def user(name):
-    return  render_template('user.html',name = name)
-
+    return render_template("user.html", name=name)
 
 
 # if __name__ == '__main__':
